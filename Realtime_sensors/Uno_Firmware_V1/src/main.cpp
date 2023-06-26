7a7cc6de7562bf0c86a9c2974fb6b0c7ec0ff8f5
Daniel <dsun@stratuscent.com>
2023-06-26 12:16:43 -0400

    First iteration of Firmware

--- Realtime_sensors/Uno_Firmware_V1/src/main.cpp
+++ Realtime_sensors/Uno_Firmware_V1/src/main.cpp
@@ -2,41 +2,73 @@
 #include "bme68xLibrary.h"
 #include "Adafruit_BME680.h"
 #include "Adafruit_Sensor.h"
-#include "Adafruit_I2CDevice.h"
 
 #define BME_SDA A4
 #define BME_SCK A5
-#define Serial_Speed 19200 //double check this later
+#define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME680 bme; //I2C
 
 
-float altitude(const float Sea_Level_Pressure= 1013.25)
-{
-}
-float Calculated_Altitude(float pressure, bool metric = true, float Sea_Level_Pressure = 101325)
-{
-  float altitude = NAN;
-  if (!isnan(pressure) && !isnan(Sea_Level_Pressure)){
-    altitude = 1000 * (Sea_Level_Pressure - pressure) / 3386.3752577878;
-  }
-  return metric ? altitude * 0.3048 : altitude;
-}
-
-float temperatureCompensatedAltitude(int32_t pressure, float temp=21.0 /*Celsius*/, float seaLevel=1013.25)
-{
-
-}
-
 void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.print(F("BME688 async test"));
+  
+   if (!bme.begin()) {
+    Serial.println(F("Could not find a valid BME688 sensor, check wiring!"));
+    while (1);
+  }
 
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+unsigned long endtime = bme.beginReading();
+
+if (endtime == 0) {
+  Serial.println(F("Failed to begin reading :("));
+  return;
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+Serial.print(F("Reading started at"));
+Serial.print(millis());
+Serial.print(F(" and will finish at "));
+Serial.println(endtime);
+
+delay(50);
+
+if (!bme.endReading()) {
+  Serial.println(F("Failed to complete reading :("));
+  return;
+}
+Serial.print(F("Reading completed at "));
+  Serial.println(millis());
+
+  Serial.print(F("Temperature = "));
+  Serial.print(bme.temperature);
+  Serial.println(F(" *C"));
+
+  Serial.print(F("Pressure = "));
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(F(" hPa"));
+
+  Serial.print(F("Humidity = "));
+  Serial.print(bme.humidity);
+  Serial.println(F(" %"));
+
+  Serial.print(F("Gas = "));
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(F(" KOhms"));
+
+  Serial.print(F("Approx. Altitude = "));
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(F(" m"));
+
+  Serial.println();
+  delay(2000);
 }
